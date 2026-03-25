4f26a8de405f583d38f3d4603813cb8daaa781ac
mochi-yu <mochi.2225.it@gmail.com>
2026-03-25 20:13:56 +0900

    fix: ベース URL の処理を修正

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,8 @@
 export const withBaseURL = (path: string) => {
   const baseURL = import.meta.env.BASE_URL;
-  return `${baseURL}/${path}`;
+  if (baseURL === "/") {
+    return `/${path}`;
+  } else {
+    return `${baseURL}/${path}`;
+  }
 };
