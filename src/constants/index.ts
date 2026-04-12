70132c01b18b65d93ec0388aa3cc912b6f2533d9
mochi-yu <mochi.2225.it@gmail.com>
2026-04-13 00:48:20 +0900

    wip: 英語用のページを追加

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,6 @@ export const constants = {
   pageTitle: "Go Conference 2026",
   pageDescription:
     "Go Conference はプログラミング言語 ”Go” ユーザーのためのカンファレンスです。",
+  PageDescriptionEn:
+    'Go Conference is a conference for users of the programming language "Go".',
 };
