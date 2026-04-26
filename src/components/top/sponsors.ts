1201b5f37ad731f1f5d4d7833d2e4d9fb7d6806c
masakurapa <rabasan1031@gmail.com>
2026-04-26 21:51:30 +0900

    スポンサーリストを追加

---
+++ src/components/top/sponsors.ts
@@ -0,0 +1,27 @@
+import type { ImageMetadata } from "astro";
+
+export type Sponsor = {
+  name: string;
+  imageSrc: ImageMetadata;
+};
+
+export const goldSponsors: Sponsor[] = [
+];
+
+export const silverSponsors: Sponsor[] = [
+];
+
+export const lunchSponsors: Sponsor[] = [
+];
+
+export const drinkSponsors: Sponsor[] = [
+];
+
+export const bronzeSponsors: Sponsor[] = [
+];
+
+export const gopherSponsors: Sponsor[] = [
+];
+
+export const toolSponsors: Sponsor[] = [
+];
