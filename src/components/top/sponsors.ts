3e04e202300372a3b05567f2010197d638ee0971
masakurapa <rabasan1031@gmail.com>
2026-06-05 23:08:11 +0900

    種別別にcomponentを分離

--- src/components/top/sponsors.ts
+++ src/components/top/sponsors.ts
@@ -1,25 +1,31 @@
 import type { ImageMetadata } from "astro";
 
-export type Sponsor = {
+export type SponsorWithDialog = {
   name: string;
   imageSrc: ImageMetadata;
-  dialog?: {
+  dialog: {
     openId: string;
     link: string;
     description: string;
   };
 };
 
-export const goldSponsors: Sponsor[] = [
+export type Sponsor = {
+  name: string;
+  imageSrc: ImageMetadata;
+};
+
+
+export const goldSponsors: SponsorWithDialog[] = [
 ];
 
-export const silverSponsors: Sponsor[] = [
+export const silverSponsors: SponsorWithDialog[] = [
 ];
 
-export const lunchSponsors: Sponsor[] = [
+export const lunchSponsors: SponsorWithDialog[] = [
 ];
 
-export const drinkSponsors: Sponsor[] = [
+export const drinkSponsors: SponsorWithDialog[] = [
 ];
 
 export const bronzeSponsors: Sponsor[] = [
