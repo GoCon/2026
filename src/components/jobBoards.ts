cf1c649cb301e5f4d7b8c8fc5b9d408b5db44a31
mochi-yu <mochi.2225.it@gmail.com>
2026-06-10 01:02:12 +0900

    fix: URL を必須に

--- src/components/jobBoards.ts
+++ src/components/jobBoards.ts
@@ -3,7 +3,7 @@ import type { ImageMetadata } from "astro";
 type JobBoard = {
   image: ImageMetadata;
   description: string;
-  url?: string;
+  url: string;
 };
 
 export const jobBoards: JobBoard[] = [];
