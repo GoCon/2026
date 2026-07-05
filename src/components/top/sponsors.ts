6fc3b24ee73fbf509c49d146793bd430f33abed5
masakurapa <rabasan1031@gmail.com>
2026-07-05 22:51:22 +0900

    rm unused component

--- src/components/top/sponsors.ts
+++ src/components/top/sponsors.ts
@@ -12,50 +12,23 @@ export type Sponsor = {
 };
 
 
-import imageA from '../../assets/0_V001.png';
-import imageB from '../../assets/1_S101.png';
-import imageC from '../../assets/2_S102.png';
+export const goldSponsors: SponsorWithUrl[] = [
+];
 
-import gopher from '../../assets/page-header-gopher.png';
+export const silverSponsors: SponsorWithUrl[] = [
+];
 
-const images = [imageA, imageB, imageC];
-const img = (i: number) => images[i % 3];
+export const drinkSponsors: SponsorWithUrl[] = [
+];
 
-export const goldSponsors: SponsorWithUrl[] = Array.from({ length: 6 }, (_, i) => ({
-  name: `Gold Sponsor ${i + 1}`,
-  imageSrc: img(i),
-  link: "https://example.com",
-}));
+export const lunchSponsors: SponsorWithUrl[] = [
+];
 
-export const silverSponsors: SponsorWithUrl[] = Array.from({ length: 12 }, (_, i) => ({
-  name: `Silver Sponsor ${i + 1}`,
-  imageSrc: img(i),
-  link: "https://example.com",
-}));
+export const bronzeSponsors: Sponsor[] = [
+];
 
-export const drinkSponsors: SponsorWithUrl[] = Array.from({ length: 1 }, (_, i) => ({
-  name: `Drink Sponsor ${i + 1}`,
-  imageSrc: img(i),
-  link: "https://example.com",
-}));
+export const gopherSponsors: Sponsor[] = [
+];
 
-export const lunchSponsors: SponsorWithUrl[] = Array.from({ length: 2 }, (_, i) => ({
-  name: `Lunch Sponsor ${i + 1}`,
-  imageSrc: img(i),
-  link: "https://example.com",
-}));
-
-export const bronzeSponsors: Sponsor[] = Array.from({ length: 50 }, (_, i) => ({
-  name: `Bronze Sponsor ${i + 1}`,
-  imageSrc: img(i),
-}));
-
-export const gopherSponsors: Sponsor[] = Array.from({ length: 20 }, (_, i) => ({
-  name: `Gopher ${i + 1}`,
-  imageSrc: gopher,
-}));
-
-export const toolSponsors: Sponsor[] = Array.from({ length: 1 }, (_, i) => ({
-  name: `Tool Sponsor ${i + 1}`,
-  imageSrc: img(i),
-}));
+export const toolSponsors: Sponsor[] = [
+];
