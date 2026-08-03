f6af50e5f90bb4742d9912ff080dee58a0a63f22
mochi-yu <mochi.2225.it@gmail.com>
2026-08-04 01:03:27 +0900

    feat: シルバースポンサーのロゴを追加

--- src/components/top/sponsors.ts
+++ src/components/top/sponsors.ts
@@ -5,6 +5,15 @@ import gold3Img from "../../assets/sponsors/gold/gold_3.png";
 import gold4Img from "../../assets/sponsors/gold/gold_4.png";
 import gold5Img from "../../assets/sponsors/gold/gold_5.png";
 import gold6Img from "../../assets/sponsors/gold/gold_6.png";
+import silver2Img from "../../assets/sponsors/silver/silver_2.png";
+import silver3Img from "../../assets/sponsors/silver/silver_3.png";
+import silver4Img from "../../assets/sponsors/silver/silver_4.png";
+import silver5Img from "../../assets/sponsors/silver/silver_5.png";
+import silver6Img from "../../assets/sponsors/silver/silver_6.png";
+import silver7Img from "../../assets/sponsors/silver/silver_7.png";
+import silver8Img from "../../assets/sponsors/silver/silver_8.png";
+import silver10Img from "../../assets/sponsors/silver/silver_10.png";
+import silver11Img from "../../assets/sponsors/silver/silver_11.png";
 import gopher3Img from "../../assets/sponsors/gopher/gopher_3.png";
 import gopher5Img from "../../assets/sponsors/gopher/gopher_5.png";
 import gopher7Img from "../../assets/sponsors/gopher/gopher_7.jpg";
@@ -65,6 +74,55 @@ export const goldSponsors: SponsorWithUrl[] = [
 ];
 
 export const silverSponsors: SponsorWithUrl[] = [
+  // { name: "株式会社CARTA HOLDINGS", imageSrc: silver1Img, link: "" },
+  {
+    name: "株式会社Datachain",
+    imageSrc: silver2Img,
+    link: "https://careers.datachain.jp/",
+  },
+  {
+    name: "合同会社DMM.com",
+    imageSrc: silver3Img,
+    link: "https://dmm-corp.com/",
+  },
+  {
+    name: "株式会社Finatextホールディングス",
+    imageSrc: silver4Img,
+    link: "",
+  },
+  {
+    name: "株式会社miive",
+    imageSrc: silver5Img,
+    link: "https://careers.miive.jp/",
+  },
+  {
+    name: "REALITY株式会社",
+    imageSrc: silver6Img,
+    link: "https://reality.inc/",
+  },
+  {
+    name: "株式会社U-NEXT",
+    imageSrc: silver7Img,
+    link: "https://www.unext.co.jp/",
+  },
+  {
+    name: "株式会社エブリー",
+    imageSrc: silver8Img,
+    link: "https://corp.every.tv/",
+  },
+  // { name: "エムスリー株式会社", imageSrc: silver9Img, link: "" },
+  {
+    name: "株式会社カオナビ",
+    imageSrc: silver10Img,
+    link: "https://corp.kaonavi.jp/",
+  },
+  {
+    name: "株式会社サイバーエージェント",
+    imageSrc: silver11Img,
+    link: "https://hrmos.co/pages/cyberagent-group",
+  },
+  // { name: "フリー株式会社", imageSrc: silver12Img, link: "" },
+  // { name: "株式会社ベースマキナ", imageSrc: silver13Img, link: "" },
 ];
 
 export const drinkSponsors: SponsorWithUrl[] = [
